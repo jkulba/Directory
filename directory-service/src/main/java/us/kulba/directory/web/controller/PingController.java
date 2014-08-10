bfa114e5d52a9d0ad150e4400947493608ed29f3
James Kulba <jkulba@gmail.com>
2014-08-09 20:54:41 -0700

    + Add Serializer package

---
+++ directory-service/src/main/java/us/kulba/directory/web/controller/PingController.java
@@ -0,0 +1,27 @@
+package us.kulba.directory.web.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import us.kulba.directory.model.Ping;
+
+@RestController
+public class PingController {
+    final Logger logger = LoggerFactory.getLogger(PingController.class);
+
+    @RequestMapping("/ping")
+    public @ResponseBody
+    Ping ping() {
+
+        logger.debug("Hit PingController.ping()");
+
+        Ping p = new Ping();
+        p.setApplication("Crayon Service");
+        p.setVersion("1.0");
+
+        return p;
+
+    }
+}
